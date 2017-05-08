1be2195827261e44db9a49e5d359cf58f20eee01
flederspinne <taillikeapistol@gmail.com>
2017-05-08 20:06:05 +0300

    Add files via upload
    Я пока не особо понимаю, как коммитить таким образом, чтобы изменения отображались прямо здесь и не требовали ручной дозаливки файлов.

--- functions.js
+++ functions.js
@@ -22,24 +22,51 @@ function do_something(who, what) {
     switch (what) {
         case 1: {
             who.come_to_office();
+            break;
         }
         case 2: {
             who.go_home();
+            break;
         }
         case 3: {
             who.drink("кофе");
+            break;
         }
         case 4: {
             who.laugh();
+            break;
         }
         case 5: {
             who.read_documentation();
+            break;
         }
         case 6: {
             who.talk();
+            break;
         }
         case 7: {
             who.code();
+            break;
         }
     }
+}
+
+function working_day(who) {
+    setInterval(function () {
+
+        if (!flag_come) {
+            do_something(who, 1);
+            flag_come = true;
+        }
+
+        let rand_method = Math.floor(Math.random() * (Object.getOwnPropertyNames(who).length - 2) + 3);
+
+        do_something(who, rand_method);
+
+    }, 2000);
+}
+
+function add_coder(where, name, skill, ...language) {
+    where[where.length] = new Coder(name, skill, ...language);
+    alert(JSON.stringify(where[where.length - 1]));
 }
